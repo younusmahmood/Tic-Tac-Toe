17572b8279a83d0e9995bf83aa445555d69f7719
younusmahmood <younus23@gmail.com>
2016-06-03 22:46:59 -0500

    Update Client.java

--- TTT/src/Client.java
+++ TTT/src/Client.java
@@ -1,64 +1,70 @@
-import java.awt.Container;
-import java.awt.FlowLayout;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.io.*;
-import java.net.*;
-import java.util.*;
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
 
-import javax.swing.*;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+
+
+
 
 @SuppressWarnings("serial")
-public class Client extends JFrame implements ActionListener {
-
+public class Client extends JPanel implements ActionListener {
+	
+	
 	private boolean begin = false;
-	static ObjectOutputStream output;
-	static ObjectInputStream input;
-	static PrintWriter out;
-	static BufferedReader in;
-	static Socket socket;
-	static String name = "";
+	
+	// only one of these values will be set to true in each thread
+	// ie. player X's thread will have ImX set to true and ImO will be set to false (vice versa in player O's thread)
+	private boolean ImX = false; 
+	private boolean ImO = false; 
+	boolean btn = false;
+	
+	private boolean gameOver = false;
+	private String whosTurn = "X"; // indicate who's turn it is
+//	private String move = "";
+	private boolean move = true; // to hold the value of the players move (b1, b2, etc)
 
-	static JFrame frame;
-	private Container panel;
-	private JButton B1;
-	private JButton B2;
-	private JButton B3;
-	private JButton B4;
-	private JButton B5;
-	private JButton B6;
-	private JButton B7;
-	private JButton B8;
-	private JButton B9;
+	private ObjectOutputStream output;
+	private ObjectInputStream input;
+	private Socket socket;
+	String name;
+	String buttonPressed = "";
+	boolean player1 = false;
+	boolean player2 = true;
+	
+	private static JFrame frame;
+	private JButton connectBtn = new JButton("Connect to Opponent");
+	
 
+	private JTextArea playerRole = new JTextArea();
+	private JButton B1 = new JButton("");
+	private JButton B2 = new JButton("");
+	private JButton B3 = new JButton("");
+	private JButton B4 = new JButton("");
+	private JButton B5 = new JButton("");
+	private JButton B6 = new JButton("");
+	private JButton B7 = new JButton("");
+	private JButton B8 = new JButton("");
+	private JButton B9 = new JButton("");
+	
+	
 	public Client() {
-		frame = new JFrame("Tic Tac Toe " + name);
-		B1 = new JButton("");
-		B2 = new JButton("");
-		B3 = new JButton("");
-		B4 = new JButton("");
-		B5 = new JButton("");
-		B6 = new JButton("");
-		B7 = new JButton("");
-		B8 = new JButton("");
-		B9 = new JButton("");
-
-		B1.addActionListener(this);
-		B2.addActionListener(this);
-		B3.addActionListener(this);
-		B4.addActionListener(this);
-		B5.addActionListener(this);
-		B6.addActionListener(this);
-		B7.addActionListener(this);
-		B8.addActionListener(this);
-		B9.addActionListener(this);
-
-		panel = getContentPane();
-		panel.setLayout(new GridLayout(3, 3, 2, 2));
-		// role = new JLabel(name);
+		
+	    JPanel panel = new JPanel(new GridLayout(4, 3));
+	    
+	    panel.add(connectBtn);
+		panel.add(playerRole);
 		panel.add(B1);
 		panel.add(B2);
 		panel.add(B3);
@@ -68,140 +74,226 @@ public class Client extends JFrame implements ActionListener {
 		panel.add(B7);
 		panel.add(B8);
 		panel.add(B9);
-		// panel.add(role);
+		
+	    setLayout(new BorderLayout());
+	    add(panel, BorderLayout.CENTER);
+	    add(playerRole, BorderLayout.SOUTH);
+	    add(connectBtn, BorderLayout.NORTH);
+	    
+//	    playerRole.setHorizontalAlignment(SwingConstants.RIGHT);
+	    playerRole.setPreferredSize(new Dimension(300, 75));
+	    playerRole.setEditable(false);
+	    connectBtn.setPreferredSize(new Dimension(300, 30));
 
-		frame.add(panel);
-		frame.setSize(875, 550);
+		connectBtn.addActionListener(this);
+		B1.addActionListener(this);
+		B2.addActionListener(this);
+		B3.addActionListener(this);
+		B4.addActionListener(this);
+		B5.addActionListener(this);
+		B6.addActionListener(this);
+		B7.addActionListener(this);
+		B8.addActionListener(this);
+		B9.addActionListener(this);
+		
+		B1.setEnabled(false);
+		B2.setEnabled(false);
+		B3.setEnabled(false);
+		B4.setEnabled(false);
+		B5.setEnabled(false);
+		B6.setEnabled(false);
+		B7.setEnabled(false);
+		B8.setEnabled(false);
+		B9.setEnabled(false);
+
+
+	}
+	
+	public void actionPerformed(ActionEvent e) 
+	{
+	    Object source = e.getSource();
+	    btn = true;
+	    
+	    if (source == connectBtn) 
+	    {	
+	    	try {
+	    		connectBtn.setEnabled(false);
+	    		
+	    		ClientThread t1 = new ClientThread();
+	    		
+	    		new Thread(t1).start();
+	    		
+	    	} catch (Exception ex) 
+	    	{
+	    		System.out.println("ERROR: " + ex.getMessage());
+	    	}
+	    }
+	    
+	    if (source == B1) { 
+	    	B1.setText(whosTurn);
+	    	buttonPressed = "B1";
+	    	System.out.println("B1 pressed");
+	    }else if(source == B2){
+	    	B2.setText(whosTurn);
+	    	buttonPressed = "B2";
+	    	move = true;
+	    	System.out.println("B2 pressed");
+	    }
+	    else if(source == B3){
+	    	B3.setText(whosTurn);
+	    	buttonPressed = "B3";
+	    	move = true;
+	    	System.out.println("B3 pressed");
+	    }
+	}
+
+	/**
+	 * 
+	 * @author 
+	 *
+	 */
+    class ClientThread implements Runnable 
+    {
+    		
+		@SuppressWarnings({ "unchecked", "deprecation" })
+		public void run() {
+			String turn;
+		try {
+			socket = new Socket("localhost", 3001);
+			output = new ObjectOutputStream(socket.getOutputStream());
+			input = new ObjectInputStream(socket.getInputStream());
+		
+
+			while (!gameOver) {
+				String inputString = input.readUTF();
+				System.out.println(inputString);
+				playerRole.setText(inputString);
+				if (inputString.startsWith("Player")) { // wait for players to join / assign roles
+					name = inputString.substring(7);
+					frame.setTitle("Welcome to Tic Tac Toe: Player "+ name);
+
+					if (name.equals("X")) { // let the thread know who it belongs to (aka this thread belongs to player X)
+						System.out.println("IMx should be true now -> " + name);
+						ImX = true;
+						frame.setTitle("Welcome to Tic Tac Toe: Player "+ name);
+						enableButtons();
+						whosTurn = "X";
+					}
+					if (name.equals("O")) {
+						System.out.println("IMo should be true now -> " + name);
+						ImO = true;
+						enableButtons();
+						frame.setTitle("Welcome to Tic Tac Toe: Player "+ name);
+					}
+					playerRole.setText("Welcome to Tic Tac Toe: Player "+ name);
+					break;
+				} 
+			}
+			
+			
+			/*********************
+			 * 
+			 * 
+			 * Under here is where he error shows up
+			 * 
+			 *********************/
+			System.out.println("outside of loop now");
+			
+			while (true){
+				
+				turn = input.readUTF();
+				
+				if(turn.startsWith("")){
+					buttonPressed = turn.substring(6);
+					whosTurn = turn.substring(4, 5);
+				}
+
+//				if(buttonPressed == "B1" && B1.getText().equals("")){
+//					B1.setText(whosTurn);
+//					move = !move;
+//					B1.setEnabled(false);
+//				}
+				
+				while (buttonPressed.equals(""));
+				
+				//System.out.println("button equals: "+ buttonPressed);
+				output.writeUTF(buttonPressed + " " + whosTurn);
+				output.flush();
+				
+//				System.out.println("This button equals: "+ buttonPressed);
+//				output.writeUTF(buttonPressed + " " + whosTurn);
+//				output.flush();			   
+//				buttonPressed = "";
+				
+				
+				
+				
+			 // System.out.println(turn);
+				
+				
+				
+			}
+			
+			
+			
+        } catch (EOFException eof)
+        {
+        	System.out.println("EOF: " + eof.getMessage());
+        	
+        } catch (IOException exception) {
+        	
+        	playerRole.setText("Error: " + exception.getMessage());
+            System.out.println(exception.getMessage());
+        	connectBtn.setEnabled(true);
+        	
+        }
+		
+	}
+		
+
+	/**
+	 * enable/disable buttons 
+	 * depending on who's turn
+	 */
+	public void enableButtons() {
+		
+		B1.setEnabled(true);
+		B2.setEnabled(true);
+		B3.setEnabled(true);
+		B4.setEnabled(true);
+		B5.setEnabled(true);
+		B6.setEnabled(true);
+		B7.setEnabled(true);
+		B8.setEnabled(true);
+		B9.setEnabled(true);
+
+	}
+	public void disableButtons() {
+		
+		B1.setEnabled(false);
+		B2.setEnabled(false);
+		B3.setEnabled(false);
+		B4.setEnabled(false);
+		B5.setEnabled(false);
+		B6.setEnabled(false);
+		B7.setEnabled(false);
+		B8.setEnabled(false);
+		B9.setEnabled(false);
+	
+	}
+	
+    } 
+    
+	public static void main(String[] args) {
+		
+        frame = new JFrame("Tic Tac Toe Online");
+        frame.add(new Client());
+        frame.setSize(500, 500);
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
+		frame.setResizable(true);
 
 	}
 
-	public static void main(String[] args) {
-		TicTacToeThread client = new TicTacToeThread();
-
-		new Thread(client).start();
-		new Client();
-	}
-
-	static class TicTacToeThread implements Runnable {
-
-
-
-		@Override
-		public void run() {
-				try {
-					socket = new Socket("localhost", 3001);
-					output = new ObjectOutputStream(socket.getOutputStream());
-					input = new ObjectInputStream(socket.getInputStream());
-					
-					
-				while (true) {
-					String inputString = input.readUTF();
-					System.out.println(inputString);
-
-					if (inputString.startsWith("Player")) {
-						name = inputString.substring(7);
-						System.out.println(name);
-						frame.setTitle("Welcome to Tic Tac Toe: Player "+ name);
-					}
-					
-				}
-				
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-		}
-
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		JButton clicked = (JButton) e.getSource();
-		try {
-			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			
-			out = new PrintWriter(socket.getOutputStream(), true);
-			while (true){
-			if(clicked == B1){
-				out.println("1 " + name + " " + "B1");
-			}else if(clicked == B2){
-				out.println("2" + name + " " + "B1");
-			}else if(clicked == B3){
-				out.println("3" + name + " " + "B1");
-			}else if(clicked == B4){
-				out.println("4" + name + " " + "B1");
-			}else if(clicked == B5){
-				out.println("5" + name + " " + "B1");
-			}else if(clicked == B6){
-				out.println("6" + name + " " + "B1");
-			}else if(clicked == B7){
-				out.println("7" + name + " " + "B1");
-			}else if(clicked == B8){
-				out.println("8" + name + " " + "B1");
-			}else if(clicked == B9){
-				out.println("9" + name + " " + "B1");
-			}
-			
-			String command = in.readLine();
-			String button = command.substring(2);
-			String currentPlayer = command.substring(0, 1);
-			
-			if(command.contains("1")){
-				B1.setText(currentPlayer);
-				B1.setEnabled(false);
-			}else if(command.contains("2")){
-				B2.setText(currentPlayer);
-				B2.setEnabled(false);
-			}else if(command.contains("3")){
-				B3.setText(currentPlayer);
-				B3.setEnabled(false);
-			}else if(command.contains("4")){
-				B4.setText(currentPlayer);
-				B4.setEnabled(false);
-			}else if(command.contains("5")){
-				B5.setText(currentPlayer);
-				B5.setEnabled(false);
-			}else if(command.contains("6")){
-				B6.setText(currentPlayer);
-				B6.setEnabled(false);
-			}else if(command.contains("7")){
-				B7.setText(currentPlayer);
-				B7.setEnabled(false);
-			}else if(command.contains("8")){
-				B8.setText(currentPlayer);
-				B8.setEnabled(false);
-			}else if(command.contains("9")){
-				B9.setText(currentPlayer);
-				B9.setEnabled(false);
-			}
-				
-				
-			}
-			
-			
-			
-			
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}finally{
-			try {
-				socket.close();
-			} catch (IOException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-		}
-		
-		
-		
-		
-		
-	
-		
-		
-	}
-
-}
+} // end Client class
