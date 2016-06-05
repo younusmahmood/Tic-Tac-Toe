8892099e4f0bce110214987528a513bcdb8ba1aa
younusmahmood <younus23@gmail.com>
2016-06-04 21:16:05 -0500

    Update Client.java

--- TTT/src/Client.java
+++ TTT/src/Client.java
@@ -31,8 +31,10 @@ public class Client extends JPanel implements ActionListener {
 
 	String name;
 	static String buttonPressed = "";
-	boolean player1 = true;
-	boolean player2 = false;
+	boolean ImPlayer1 = false;
+	boolean ImPlayer2 = false;
+	boolean commandReceived = false;
+	boolean nextRound = false;
 
 	private static JFrame frame;
 	private JButton connectBtn = new JButton("Connect to Opponent");
@@ -99,7 +101,7 @@ public class Client extends JPanel implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		Object source = e.getSource();
 		btn = true;
-
+		playerRole.setText("");
 		if (source == connectBtn) {
 			try {
 				connectBtn.setEnabled(false);
@@ -115,22 +117,31 @@ public class Client extends JPanel implements ActionListener {
 
 		if (source == B1) {
 			buttonPressed = "B1";
+			B1.setText(whosTurn);
 		} else if (source == B2) {
 			buttonPressed = "B2";
+			B2.setText(whosTurn);
 		} else if (source == B3) {
 			buttonPressed = "B3";
+			B3.setText(whosTurn);
 		} else if (source == B4){
 			buttonPressed = "B4";
+			B4.setText(whosTurn);
 		} else if (source == B5) {
 			buttonPressed = "B5";
+			B5.setText(whosTurn);
 		} else if (source == B6) {
 			buttonPressed = "B6";
+			B6.setText(whosTurn);
 		} else if (source == B7) {
 			buttonPressed = "B7";
+			B7.setText(whosTurn);
 		} else if (source == B8) {
 			buttonPressed = "B8";
+			B8.setText(whosTurn);
 		} else if (source == B9) {
 			buttonPressed = "B9";
+			B9.setText(whosTurn);
 		}
 
 	}
@@ -152,47 +163,176 @@ public class Client extends JPanel implements ActionListener {
 						frame.setTitle("Welcome to Tic Tac Toe: Player " + name);
 
 						if (name.equals("X")) {
-							System.out.println("IMx should be true now -> " + name);
+//							System.out.println("IMx should be true now -> " + name);
 							frame.setTitle("Welcome to Tic Tac Toe: Player " + name);
-							enableButtons();
-							whosTurn = "X";
+							ImPlayer1 = true;
+//							System.out.println("also, I'm player1 = " + ImPlayer1);
+							
 						}
 						if (name.equals("O")) {
-							System.out.println("IMo should be true now -> " + name);
-							enableButtons();
+//							System.out.println("IMo should be true now -> " + name);
 							frame.setTitle("Welcome to Tic Tac Toe: Player " + name);
+							ImPlayer2 = true;
+//							System.out.println("also, I'm player2 = " + ImPlayer2);
 						}
 						playerRole.setText("Welcome to Tic Tac Toe: Player " + name);
+						nextRound = true;
 						break;
 					}
 				}
 
-				String turn;
-
 				try {
+				String chatInput;
+				String[] serverCommands = {"F", "F"};
+		        while ((chatInput = input.readUTF()) != null) {
+		        	if (chatInput.contains(" ")) {
+		        		serverCommands = chatInput.split(" ");
+		        		whosTurn = serverCommands[2];
+		        		System.out.println("server says: Player " + serverCommands[0] + " pressed " + serverCommands[1]);
+		        		commandReceived = true;
+		        	}
+		        	if (chatInput.contains("Begin")) {
+		        		System.out.println("server says: " + chatInput);
+		        		whosTurn = "X";
+		        	}
+		        	System.out.println("It is Player " + whosTurn + "'s turn.");
+		        	if (whosTurn.equals("X")) {
+		        		
+		        		if (ImPlayer1) { // its my turn: need to make a move, send to server, wait for my turn again
+		        			updateBoard(serverCommands[1], serverCommands[0]);
+		        			commandReceived = false;		        			
+
+		        			playerRole.setText("Your move :)");
+		        			enableButtons();
+							while (buttonPressed.equals("")) {
+								Thread.sleep(2000);
+							}
+//							while (buttonPressed.equals(""));
+							System.out.println(buttonPressed +" Pressed! Sending to Server.");
+							output.writeUTF(buttonPressed);
+							output.flush();
+							buttonPressed = "";
+							Thread.sleep(2000);
+		        		}
+		        		else if (ImPlayer2 && commandReceived) { // not my turn: need to listen for command from server, update my board, then make a move? 
+		        			System.out.println("Updating board. Setting " + serverCommands[1] + " to " + serverCommands[0]);
+		        			updateBoard(serverCommands[1], serverCommands[0]);
+		        			commandReceived = false;		        			
+		        		}
+		        		else {
+		        			System.out.println(ImPlayer1 + " or " + ImPlayer2 + " and I'm here");
+		        		}
+		        	}
+		        	
+		        	else if (whosTurn.equals("O")) {
+		        		
+		        		if (ImPlayer1) { // not my turn: need to listen for command from server, update my board, then make a move?
+		        			System.out.println("Updating board");
+		        			updateBoard(serverCommands[1], serverCommands[0]);
+		        			commandReceived = false;		        			
+		        		}
+		        		else if (ImPlayer2) { // its my turn: need to make a move, send to server, wait for my turn again
+		        			updateBoard(serverCommands[1], serverCommands[0]);
+		        			commandReceived = false;		        			
+
+		        			playerRole.setText("Your move :)");
+		        			enableButtons();
+							while (buttonPressed.equals("")) {
+								Thread.sleep(2000);
+							}
+//							while (buttonPressed.equals(""));
+							System.out.println(buttonPressed +" Pressed! Sending to Server.");
+							output.writeUTF(buttonPressed);
+							output.flush();
+							buttonPressed = "";
+							Thread.sleep(2000);
+		        		}
+
+		        	}
+		        	System.out.println("Round finished.");
+		        }
+				String turn;
+				String[] previousMove;
 
 					while (true) {
-						Thread.sleep(2000);
-						while (buttonPressed.equals(""));
-						if (move == true) {
-							whosTurn = "X";
-						} else {
-							whosTurn = "O";
+						System.out.println("AM I IN HERE?!");
+						if (nextRound = true) {
+							nextRound = false;
+							System.out.println("It is Player " + whosTurn + "'s turn.");
+							
+							if (whosTurn.equals("X")) {
+								
+								if (ImPlayer1 == true) {
+									System.out.println("Player X's thread. Your Turn.");
+									playerRole.setText("Your Move");
+									while (buttonPressed.equals("")) {
+										Thread.sleep(2000);
+									}
+									
+									while (buttonPressed.equals(""));
+									System.out.println("Button Pressed!" + buttonPressed);
+									output.writeUTF(buttonPressed);
+									output.flush();
+									buttonPressed = "";
+									Thread.sleep(2000);
+								}
+								else if (ImPlayer2 == true) {
+									System.out.println("Player O's thread. NOT YOUR TURN.");
+							        while ((turn = input.readUTF()) != null) {
+							        	System.out.println("server says: " + turn);
+										previousMove = turn.split(" ");
+										System.out.println(previousMove[0] + " " + previousMove[1] + " " + previousMove[2]);
+										updateBoard(previousMove[1], previousMove[0]);
+										whosTurn = previousMove[2];
+										if (previousMove[3].equals("true")) nextRound = true;
+										break;
+							        }
+
+								}
+							}
+							else if (whosTurn.equals("O")) { // player 2's turn
+								
+								if (ImPlayer1 == true) {
+									System.out.println("Player X's thread. NOT YOUR TURN.");
+							        while ((turn = input.readUTF()) != null) {
+							        	System.out.println("server says: " + turn);
+										previousMove = turn.split(" ");
+										System.out.println(previousMove[0] + " " + previousMove[1] + " " + previousMove[2]);
+										updateBoard(previousMove[1], previousMove[0]);
+										whosTurn = previousMove[2];
+										if (previousMove[3].equals("true")) nextRound = true;
+										break;
+							        }
+//									Thread.sleep(2000);
+//									turn = input.readUTF();
+//									System.out.println("received from server: " + turn);
+//									previousMove = turn.split(" ");
+//									System.out.println(previousMove[0] + " " + previousMove[1] + " " + previousMove[2]);
+//									updateBoard(previousMove[1], previousMove[0]);
+//									playerRole.setText("YOUR MOVE");
+//									whosTurn = previousMove[2];
+//									if (previousMove[3].equals("true")) nextRound = true;
+//									Thread.sleep(2000);
+								}
+								else if (ImPlayer2 == true) {
+									System.out.println("Player O's thread. Your Turn.");
+									playerRole.setText("Your Move");
+									while (buttonPressed.equals("")) {
+										Thread.sleep(2000);
+									}
+									while (buttonPressed.equals(""));
+									System.out.println("Button Pressed!" + buttonPressed);
+									output.writeUTF(buttonPressed);
+									output.flush();
+									buttonPressed = "";
+									Thread.sleep(2000);
+								}
+							}
 						}
 						
-						output.writeUTF(buttonPressed + " " + whosTurn);
-						output.flush();
 
-						turn = input.readUTF();
-
-						if (turn.substring(4, 5) == "X"
-								|| turn.substring(4, 5) == "O") {
-							buttonPressed = turn.substring(6);
-							whosTurn = turn.substring(4, 5);
-						}
-
-						System.out.println("server returned: " + whosTurn + " "
-								+ buttonPressed);
+//						System.out.println("server returned: " + whosTurn + " "
+//								+ buttonPressed);
 
 						if (buttonPressed == "B1" && B1.getText() == "") {
 							B1.setText(whosTurn);
@@ -209,7 +349,7 @@ public class Client extends JPanel implements ActionListener {
 						} else if (buttonPressed == "B4" && B4.getText() == "") {
 							B4.setText(whosTurn);
 							B4.setEnabled(false);
-							move = !move;
+							move = !move;	
 						} else if (buttonPressed == "B5" && B5.getText() == "") {
 							B5.setText(whosTurn);
 							B5.setEnabled(false);
@@ -231,7 +371,8 @@ public class Client extends JPanel implements ActionListener {
 							B9.setEnabled(false);
 							move = !move;
 						}
-						
+
+						//repaintButtons();
 						
 					}
 
@@ -260,6 +401,52 @@ public class Client extends JPanel implements ActionListener {
 
 		}
 
+		public void updateBoard(String buttonToUpdate, String setTextTo) {
+
+			if (buttonToUpdate.equals("F")) { // first round
+				// do nothing
+			} else {
+				System.out.println("Updating board. Setting " + buttonToUpdate + " to " + setTextTo);
+				if (buttonToUpdate.equals("B1")) {
+					B1.setText(setTextTo);
+					B1.setEnabled(false);
+				}
+				else if (buttonToUpdate.equals("B2")) {
+					B2.setText(setTextTo);
+					B2.setEnabled(false);
+				}
+				else if (buttonToUpdate.equals("B3")) {
+					B3.setText(setTextTo);
+					B3.setEnabled(false);
+				}
+				else if (buttonToUpdate.equals("B4")) {
+					B4.setText(setTextTo);
+					B4.setEnabled(false);
+				}
+				else if (buttonToUpdate.equals("B5")) {
+					B5.setText(setTextTo);
+					B5.setEnabled(false);
+				}
+				else if (buttonToUpdate.equals("B6")) {
+					B6.setText(setTextTo);
+					B6.setEnabled(false);
+				}
+				else if (buttonToUpdate.equals("B7")) {
+					B7.setText(setTextTo);
+					B7.setEnabled(false);
+				}
+				else if (buttonToUpdate.equals("B8")) {
+					B8.setText(setTextTo);
+					B8.setEnabled(false);
+				}
+				else if (buttonToUpdate.equals("B9")) {
+					B9.setText(setTextTo);
+					B9.setEnabled(false);
+				}
+			}
+
+		}
+		
 		/**
 		 * enable/disable buttons depending on who's turn
 		 */
@@ -290,12 +477,44 @@ public class Client extends JPanel implements ActionListener {
 			B9.setEnabled(false);
 
 		}
+		
+		public void repaintButtons() {
+
+			B1.revalidate();
+			B1.repaint();
+			
+			B2.revalidate();
+			B2.repaint();
+
+			B3.revalidate();
+			B3.repaint();
+			
+			B4.revalidate();
+			B4.repaint();
+
+			B5.revalidate();
+			B5.repaint();
+
+			B6.revalidate();
+			B6.repaint();
+			
+			B7.revalidate();
+			B7.repaint();
+			
+			B8.revalidate();
+			B8.repaint();
+			
+			B9.revalidate();
+			B9.repaint();
+		}
+
 
 	}
 
 	public static void playGame() throws Exception {
 
 	}
+
 
 	public static void main(String[] args) throws Exception {
 		frame = new JFrame("Tic Tac Toe Online");
